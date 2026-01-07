0ecaece8deba92d16a0c03cbc8f5c6d5a5cbbf5a
levins9 <24017226@myrp.edu.sg>
2026-01-07 15:28:54 +0800

    update

--- server.js
+++ server.js
@@ -32,3 +32,15 @@ app.get('/allcards', async (req, res) => {
         res.status(500).json({ message: 'Server error for allcards'});
     }
 })
+
+app.post('/addcard', async (req, res) => {
+    const {card_name, card_pic} =req.body;
+    try {
+        let connection = await mysql.createConnection({dbConfig});
+        await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?,?)',[card_name, card_pic]);
+        res.status (201).json({message: 'Card' + card_name + 'successfully added'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error - could not add card'+ card_name });
+    }
+})
